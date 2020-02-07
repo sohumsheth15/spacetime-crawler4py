9b169bace8aec48757a8c59709ee11363cea0bfd
Sohum Sheth <sohumsheth15@gmail.com>
2020-02-06 17:50:15 -0800

    added stuff

--- scraper.py
+++ scraper.py
@@ -1,18 +1,30 @@
 import re
 from urllib.parse import urlparse
+from urllib.parse import urldefrag
+from bs4 import BeautifulSoup
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp):
-    # Implementation requred.
-    return list()
+    content = []
+    if 200 <= resp.status <= 599:
+        data = resp.raw_response.content
+        info = BeautifulSoup(data, 'html.parser')
+        for _ in info.find_all("a"):
+            content.append(_.get('href'))
+    
+    return [urldefrag(i)[0] for i in content]
 
 def is_valid(url):
     try:
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
+            return False
+        if parsed.netloc not in set(["www.ics.uci.edu", "www.cs.uci.edu", "www.informatics.uci.edu", "www.stat.uci.edu", "www.today.uci.edu"]):
+            return False
+        if parsed.netloc == "www.today.uci.edu" and parsed.path != "/department/information_computer_sciences":
             return False
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
